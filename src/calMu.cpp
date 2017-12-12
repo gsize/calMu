bae019be51158037c1fbf1a9b2016a5085ee5b38
gsize <gsizeg@gmail.com>
2017-12-12 16:07:12 +0800

    parse GateMaterialsDB.db in calMu

--- src/calMu.cpp
+++ src/calMu.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	/*
-	 // Test read MDATX3 database
+	// Test read MDATX3 database
 	MDATX3 data;
 	char dataPath[30];
 	sprintf(dataPath,"%s",argv[1]);
@@ -20,25 +20,27 @@ int main(int argc, char** argv)
 
 	for(int i=0;i<data.LAX;i++)
 	{
-		int IMAX = data.KMX[i];
-		for(int I=0;I<IMAX;I++)
-		{
-			float fd;
-			printf("%f\t",data.PHC[i][I]);
-		}
-		printf("\n");
-		printf("%6d",data.KMX[i]);
-		printf("\n");
-		if(i== data.LAX-1)
-			printf("\n");
+	int IMAX = data.KMX[i];
+	for(int I=0;I<IMAX;I++)
+	{
+	float fd;
+	printf("%f\t",data.PHC[i][I]);
 	}
-*/
-//read Gate MaterialsDB
-MaterialDB matDB;
-matDB.ReadDB("../data/GateMaterials.db");
-matDB.PrintElements();
-matDB.PrintMaterials(1);
+	printf("\n");
+	printf("%6d",data.KMX[i]);
+	printf("\n");
+	if(i== data.LAX-1)
+	printf("\n");
+	}
+	*/
+	//read Gate MaterialsDB
+	MaterialDB matDB;
+	matDB.ReadDB("../data/GateMaterials.db");
+	matDB.PrintElements();
+	matDB.PrintMaterials(1);
+	matDB.ReadMu("../data/mumap.txt");
 
+	printf("-------------------------------------------------\n");
 	int MMAX;
 	int NZ[100];
 	float WT[100];
@@ -48,64 +50,64 @@ matDB.PrintMaterials(1);
 	MMAX =ParseFormulas(w.c_str(),NZ,WT);
 	if(MMAX<0)
 	{
-	printf("FORM status %d\n",MMAX);
-	return -1;
+		printf("FORM status %d\n",MMAX);
+		return -1;
 	}
 	for(int i=0;i<MMAX;i++)
-	printf("ele:%s MMAX: %d JZ[%d]: %d WT[%d]: %f\n",w.c_str(),MMAX,i,NZ[i],i,WT[i]);
+		printf("ele:%s MMAX: %d JZ[%d]: %d WT[%d]: %f\n",w.c_str(),MMAX,i,NZ[i],i,WT[i]);
 
 
-for(int i=0;i<matDB.m_matList.size();i++)
-{
-	int MMAX;
-	int NZ[100];
-	float WT[100];
-
-	Element *el = NULL;
-	MMAX = matDB.m_matList[i].GetNum();
-	for(int j=0;j<MMAX;j++)
+	for(int i=0;i<matDB.m_matList.size();i++)
 	{
-		el =matDB.m_matList[i].GetElement(j);
-		NZ[j] = el->m_Z;
-		WT[j] = matDB.m_matList[i].Getf(j);;
-	}
-	float wtSun=0.;
-	for(int i=0;i<MMAX;i++)
-	{
-		wtSun +=WT[i];
-	}
-	if(wtSun<0.999999)
-	{
-		printf("weigth <1.");
-	for(int i=0;i<MMAX;i++)
-	{
-		 WT[i]/=wtSun;
-	}
-	}
+		int MMAX;
+		int NZ[100];
+		float WT[100];
 
-	float EAD[600];
-	int JZ[600],JM[600];
-	int JENG =1;
-	EAD[0]=511000.0;
-	
-	float EN[1200];
-	int KZ[1200],KM[1200];
-	int lenth= 0;
+		Element *el = NULL;
+		MMAX = matDB.m_matList[i].GetNum();
+		for(int j=0;j<MMAX;j++)
+		{
+			el =matDB.m_matList[i].GetElement(j);
+			NZ[j] = el->m_Z;
+			WT[j] = matDB.m_matList[i].Getf(j);;
+		}
+		float wtSun=0.;
+		for(int i=0;i<MMAX;i++)
+		{
+			wtSun +=WT[i];
+		}
+		if(wtSun<0.999999)
+		{
+			printf("weigth <1.");
+			for(int i=0;i<MMAX;i++)
+			{
+				WT[i]/=wtSun;
+			}
+		}
 
-	int NEGO =3; //3:only input energy list 
-	int NF = 3;
-	int KMAX = MMAX;
-	lenth = InitEnergyList(KMAX,NZ,NEGO,JENG,JZ,JM,EAD,EN,KZ,KM);
+		float EAD[600];
+		int JZ[600],JM[600];
+		int JENG =1;
+		EAD[0]=511000.0;
 
-	float SCTCO[ME],SCTIN[ME],PHT[ME],PRAT[ME],PREL[ME];
-	Calculation(KMAX,NZ,WT, NF,NEGO,lenth,EN,KZ,KM,SCTCO,SCTIN,PHT,PRAT,PREL);
+		float EN[1200];
+		int KZ[1200],KM[1200];
+		int lenth= 0;
 
-	float crossAll=0.;
-	for(int j=0;j<lenth; j++)
-	{
-		crossAll =SCTCO[j]+SCTIN[j]+PHT[j]+PRAT[j]+PREL[j];
-		printf("%15s  %10.3lf  %10.6E  %10.6E\n",matDB.m_matList[i].GetName().c_str(),EN[j]/1000.,crossAll,crossAll*matDB.m_matList[i].GetDensity());
+		int NEGO =3; //3:only input energy list 
+		int NF = 3;
+		int KMAX = MMAX;
+		lenth = InitEnergyList(KMAX,NZ,NEGO,JENG,JZ,JM,EAD,EN,KZ,KM);
+
+		float SCTCO[ME],SCTIN[ME],PHT[ME],PRAT[ME],PREL[ME];
+		Calculation(KMAX,NZ,WT, NF,NEGO,lenth,EN,KZ,KM,SCTCO,SCTIN,PHT,PRAT,PREL);
+
+		float crossAll=0.;
+		for(int j=0;j<lenth; j++)
+		{
+			crossAll =SCTCO[j]+SCTIN[j]+PHT[j]+PRAT[j]+PREL[j];
+			printf("%15s  %10.3E  %10.3lf  %10.6E  %10.6E\n",matDB.m_matList[i].GetName().c_str(),matDB.m_matList[i].GetDensity(),EN[j]/1000.,crossAll,0.1*crossAll*matDB.m_matList[i].GetDensity());
+		}
 	}
-}
 	return 0;
 }
