ffd6e918b0d997be49429f12fbf6c514a3eec1f8
gsize <gsizeg@gmail.com>
2019-01-11 16:32:15 +0800

    添加参数说明

--- src/xcom.h
+++ src/xcom.h
@@ -27,10 +27,11 @@ typedef struct MDATX3{
 // read MDATX3 datebase
 int ReadMDATX3(char *file,MDATX3 *data);
 
-// Reads element symbols or chemical formulas.
-int ParseFormulas(const char *W,int *JZ,float *WT);
-
 // Sorts into monotonically increasing order 
+// sort energy list
+// input parameter:
+//    NMAX: size of E
+//    E   : pointer to  list of energy
 template <class T>
 void SORT(int NMAX, T *E);
 
@@ -54,9 +55,45 @@ int BSPOL(const T S,T *X,T *A,T *B,T *C,T *D,const int N,T &G);
 template <class T>
 int BLIN(const T S,T *X,T *Y,const int N,T &TY);
 
+//////////////////////////////////////////////////////////////
+// Reads element symbols or chemical formulas.
+// input parameter:
+//     W  :  chemical formulas
+//     JZ :  list of element shell number
+//     WT :  list of weight of element
+int ParseFormulas(const char *W,int *JZ,float *WT);
+
+// Inititialize energy list 
+// input parameter:
+//   KMAX : number of elements
+//   NZ   : list of atomic number of element
+//   NEGO : flag of output status in energy list (1: default energy list(1 keV -- 100 GeV ) ; 2:default and add energy list; 3: only input energy list,)
+//   JENG : number of add energy 
+//   JZ   : list of add energy flag
+//   JM   : list of add energy flag
+//   EAD  : list of add energy
+//   EN   : list of energy wanted to output
+//   KZ   : list of energy wanted to output flag
+//   KM   : list of energy wanted to output flag
+// return:  the number of output energy
 int InitEnergyList(int KMAX,int *NZ,int NEGO,int JENG,int *JZ,int *JM,float *EAD,float *EN,int *KZ,int *KM);
 
-//void Calculation(int KMAX,int *NZ, int NF,int NEGO,int NENG,float *EN,int *KZ,int *KM);
+// calculate the cross sections for the interactions of photons with element, compound or mixture
+// input parameter:
+//   KMAX  : number of element
+//   NZ    : list of atomic number of element
+//   WEIGHT: list of weight of element
+//   NF    :
+//   NEGO  : flag of output status in energy list (1: default energy list(1 keV -- 100 GeV ) ; 2:default and add energy list; 3: only input energy list,)
+//   NENG  : number of output energy
+//   EN    : list of energy
+//   KZ    : list of shell flag of element
+//   KM    : list
+//   SCTCO : scattering with coherent
+//   SCTIN : scattering with incoherent
+//   PHT   : photo elctric
+//   PRAT  : pair proudction in nuclear field
+//   PREL  : pair proudction in electron field
 void Calculation(int KMAX,int *NZ,float *WEIGHT, int NF,int NEGO,int NENG,float *EN,int *KZ,int *KM,float *SCTCO,float *SCTIN,float *PHT,float *PRAT,float *PREL);
 
 #endif
