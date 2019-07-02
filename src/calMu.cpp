b490dc73fb5cd7c4f5ac4e61a4eca90fa1859bea
Gao Size <gsize@163.com>
2019-07-02 17:20:19 +0800

    输出Gate材料的所有物质全能量段截面

--- src/calMu.cpp
+++ src/calMu.cpp
@@ -71,6 +71,82 @@ void AnalysisGateMaterials()
 {
     //read Gate MaterialsDB
     string gateMaterialsDB("../data/GateMaterials.db");
+    MaterialDB matDB;
+    matDB.ReadDB(gateMaterialsDB);
+
+    for(int i=0;i<matDB.m_matList.size();i++)
+    {
+        int MMAX;
+        int NZ[100];
+        float WT[100];
+
+        Element *el = NULL;
+        MMAX = matDB.m_matList[i].GetNum();
+        for(int j=0;j<MMAX;j++)
+        {
+            el =matDB.m_matList[i].GetElement(j);
+            NZ[j] = el->m_Z;
+            WT[j] = matDB.m_matList[i].Getf(j);;
+        }
+        float wtSun=0.;
+        for(int i=0;i<MMAX;i++)
+        {
+            wtSun +=WT[i];
+        }
+        if(wtSun<0.999999)
+        {
+            printf("weigth <1.");
+            for(int i=0;i<MMAX;i++)
+            {
+                WT[i]/=wtSun;
+            }
+        }
+
+        float EAD[600];
+        int JENG =1;
+        EAD[0]=511000.0;
+
+        float EN[1200];
+        int KZ[1200],KM[1200];
+        int lenth= 0;
+
+        int NEGO =2; //3:output all input energy list and default
+        int NF = 3;
+        int KMAX = MMAX;
+
+        lenth = InitEnergyList(KMAX,NZ,NEGO,JENG,EAD,EN,KZ,KM);
+
+        float SCTCO[ME],SCTIN[ME],PHT[ME],PRAT[ME],PREL[ME],PHDIF[ME];
+        Calculation(KMAX,NZ,WT, NF,NEGO,lenth,EN,KZ,KM,SCTCO,SCTIN,PHT,PRAT,PREL,PHDIF);
+
+		FILE *fp=NULL;
+		string outfile(matDB.m_matList[i].GetName());
+		outfile+="_cross.txt";
+		fp=fopen(outfile.c_str(),"w");
+		fprintf(fp,"          Photon      Scattering        Photo-     Pair production    Total attenuation\n");
+    	fprintf(fp,"          energy   coherent  incoher.  electric     in        in       with     without\n");
+    	fprintf(fp,"                                       absorption  nuclear electron  coherent  coherent\n");
+    	fprintf(fp,"                                                    field    field     scatt.    scatt.\n");
+    	fprintf(fp,"          (MeV)    (cm2/g)    (cm2/g)   (cm2/g)    (cm2/g)  (cm2/g)   (cm2/g)   (cm2/g)\n");
+    	for(int j=0;j<lenth; j++)
+    	{
+    	    float crossAll =SCTCO[j]+SCTIN[j]+PHT[j]+PRAT[j]+PREL[j];
+    	    double ca_noco=SCTIN[j]+PHT[j]+PRAT[j]+PREL[j];
+    	    if(KZ[j]>0)
+    	    {
+    	        fprintf(fp,"       %10.3E%10.3E%10.3E%10.3E%10.3E%10.3E%10.3E%10.3E\n",EN[j]*1.0e-6,SCTCO[j],SCTIN[j],PHT[j]-PHDIF[j],PRAT[j],PREL[j],crossAll-PHDIF[j],ca_noco-PHDIF[j]);
+    	        fprintf(fp,"%7d%10.3E%10.3E%10.3E%10.3E%10.3E%10.3E%10.3E%10.3E\n",KZ[j],EN[j]*1.0e-6,SCTCO[j],SCTIN[j],PHT[j],PRAT[j],PREL[j],crossAll,ca_noco);
+    	    }else
+    	        fprintf(fp,"       %10.3E%10.3E%10.3E%10.3E%10.3E%10.3E%10.3E%10.3E\n",EN[j]*1.0e-6,SCTCO[j],SCTIN[j],PHT[j]-PHDIF[j],PRAT[j],PREL[j],crossAll-PHDIF[j],ca_noco-PHDIF[j]);
+    	}
+		fclose(fp);
+    }
+}
+
+void AnalysisGateMaterials_511keV()
+{
+    //read Gate MaterialsDB
+    string gateMaterialsDB("../data/GateMaterials.db");
     string muFile("../data/mumap.txt");
     MaterialDB matDB;
     matDB.ReadDB(gateMaterialsDB);
@@ -150,6 +226,7 @@ int main(int argc, char** argv)
         AnalysisChemicalFormulas(argv[1]);
     else
         AnalysisGateMaterials();
+        //AnalysisGateMaterials_511keV();
 
     return 0;
 }
